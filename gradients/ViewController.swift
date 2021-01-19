9b989e7c76492b3e3585975c574c12ab2d0da41c
Alex <drr1se.am@gmail.com>
2021-01-19 14:43:24 +0500

    Initial Commit

---
+++ gradients/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gradients
+//
+//  Created by Алексей Муравейник on 19.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
