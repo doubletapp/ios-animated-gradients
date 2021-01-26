e9f571a48e73cd7399607b89d412b21a1216c694
Alex <drr1se.am@gmail.com>
2021-01-27 03:31:25 +0500

    Freeform gradient

--- gradients/GradientView.swift
+++ gradients/GradientView.swift
@@ -9,6 +9,21 @@ struct PixelData {
     var b: UInt8
 }
 
+struct FactorData {
+    var r: CGFloat
+    var g: CGFloat
+    var b: CGFloat
+    var f: CGFloat
+}
+
+struct PointMeta {
+    let dist: CGFloat
+    let slope: CGFloat
+    let intercept: CGFloat
+    let left: Bool
+    let pixel: PixelData
+}
+
 class GradientView: UIView {
     
     let paleYellow = PixelData(a: 255, r: 253, g: 245, b: 203)
@@ -18,52 +33,73 @@ class GradientView: UIView {
     
     let c = PixelData(a: 255, r: 0, g: 255, b: 255)
     let m = PixelData(a: 255, r: 255, g: 0, b: 255)
-    let yi = PixelData(a: 255, r: 255, g: 255, b: 0)
+    let y = PixelData(a: 255, r: 255, g: 255, b: 0)
     let k = PixelData(a: 255, r: 0, g: 0, b: 0)
     
-    let с4 = CGPoint(x: 0, y: 0)
-    let с3 = CGPoint(x: 0, y: UIScreen.main.bounds.height)
-    let с2 = CGPoint(x: UIScreen.main.bounds.width, y: UIScreen.main.bounds.height)
-    let с1 = CGPoint(x: UIScreen.main.bounds.width, y: 0)
+    var c1 = CGPoint(x: UIScreen.main.bounds.width - 75, y: 75)
+    var c2 = CGPoint(x: 150, y: 250)
+    
+    var c3 : CGPoint {
+        return CGPoint(x: UIScreen.main.bounds.width - c1.x, y: UIScreen.main.bounds.height - c1.y)
+    }
+    
+    var c4 : CGPoint {
+        return CGPoint(x: UIScreen.main.bounds.width - c2.x, y: UIScreen.main.bounds.height - c2.y)
+    }
     
     override func draw(_ rect: CGRect) {
         let width = Int(UIScreen.main.bounds.width)
         let height = Int(UIScreen.main.bounds.height)
         
-        print("width: \(width), height: \(height)")
-        
+        let points = [
+            (c1, darkGreen),
+            (c2, yellow),
+            (c3, green),
+            (c4, paleYellow)
+        ]
+                
         var colors = [PixelData]()
         
         for y in 0..<height {
-            
-            let bottomCoeff = CGFloat(y) / UIScreen.main.bounds.height
-            let topCoeff = 1 - bottomCoeff
-            
             for x in 0..<width {
-                            
-                let rightCoeff = CGFloat(x) / UIScreen.main.bounds.width
-                let leftCoeff = 1 - rightCoeff
-                
-                let topR = CGFloat(c.r) * leftCoeff + CGFloat(m.r) * rightCoeff
-                let topG = CGFloat(c.g) * leftCoeff + CGFloat(m.g) * rightCoeff
-                let topB = CGFloat(c.b) * leftCoeff + CGFloat(m.b) * rightCoeff
-                
-                let bottomR = CGFloat(k.r) * leftCoeff + CGFloat(yi.r) * rightCoeff
-                let bottomG = CGFloat(k.g) * leftCoeff + CGFloat(yi.g) * rightCoeff
-                let bottomB = CGFloat(k.b) * leftCoeff + CGFloat(yi.b) * rightCoeff
-                
-                let pixel = PixelData(
-                    a: 255,
-                    r: UInt8(topR * topCoeff + bottomR * bottomCoeff),
-                    g: UInt8(topG * topCoeff + bottomG * bottomCoeff),
-                    b: UInt8(topB * topCoeff + bottomB * bottomCoeff)
-                )
-                
-                colors.append(pixel)
-                
-                if x == 400, y == 300 {
-                    print(pixel)
+
+                let pointsMeta = points.map({ (p, pixel) -> PointMeta in
+                    
+                    let slope = -(p.y - CGFloat(y)) / (p.x - CGFloat(x))
+                    let intercept = p.x * slope - p.y
+                    
+                    return PointMeta(
+                        dist: dist(x1: p.x, x2: CGFloat(x), y1: p.y, y2: CGFloat(y)),
+                        slope: slope,
+                        intercept: intercept,
+                        left: CGFloat(y) <= (slope * CGFloat(x) + intercept),
+                        pixel: pixel
+                    )
+                }).sorted { a, b in
+                    a.dist > b.dist
                 }
+                                
+                let result = pointsMeta.reduce(
+                    FactorData(r: 0, g: 0, b: 0, f: 0)
+                ) { (t, c) in
+                    let maximum = max(1 - (c.dist / CGFloat(width)), 0)
+                    let factor = maximum * maximum
+                    return FactorData(
+                        r: t.r + CGFloat(c.pixel.r) * factor,
+                        g: t.g + CGFloat(c.pixel.g) * factor,
+                        b: t.b + CGFloat(c.pixel.b) * factor,
+                        f: t.f + factor
+                    )
+                }
+                
+                colors.append(
+                    PixelData(
+                        a: 255,
+                        r: UInt8(result.r / result.f),
+                        g: UInt8(result.g / result.f),
+                        b: UInt8(result.b / result.f)
+                    )
+                )
             }
         }
         
@@ -73,6 +109,12 @@ class GradientView: UIView {
         }
         
         image.draw(at: .zero)
+    }
+    
+    func dist(x1: CGFloat, x2: CGFloat, y1: CGFloat, y2: CGFloat) -> CGFloat {
+        let x = CGFloat(x1 - x2)
+        let y = CGFloat(y1 - y2)
+        return sqrt(x * x + y * y)
     }
     
     func imageFromARGB32Bitmap(pixels: [PixelData], width: Int, height: Int) -> UIImage? {
